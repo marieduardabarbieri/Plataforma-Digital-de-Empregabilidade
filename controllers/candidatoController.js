032ae5c71f7212eea44e15fd053a6cfef75a010d
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-09-03 10:46:43 -0400

    Adição de candidatos no seeder para melhor visualização dos candidatos na tela de candididaturas recebidas

--- controllers/candidatoController.js
+++ controllers/candidatoController.js
@@ -1,7 +1,7 @@
 const { Candidato, Curriculo } = require('../models');
 
 const getCandidato = async function (req, res, next) {
-    const candidatos = await Candidato.findAll({
+    const candidato = await Candidato.findByPk(1, { //para buscar somente o usuario 1 enquanto não tem as verificações de login para mostrar somente no perfil o usuario 
         include: [
             {
                 model: Curriculo,
@@ -10,7 +10,7 @@ const getCandidato = async function (req, res, next) {
         ],
     });
 
-    res.render('candidatos', { candidatos })
+    res.render('candidatos/index', { candidato })
 };
 
 const cadastroCandidato = async function (req, res) {
--- seeders/20260825141050-candidatos.js
+++ seeders/20260825141050-candidatos.js
@@ -13,6 +13,36 @@ module.exports = {
         telefone: '(67) 99980-9654',
         cidade_candidato: 'São Paulo',
         uf_candidato: "SP"
+      },
+      {
+        nome: 'João Silva',
+        email: 'joao@gmail.com',
+        senha: '123',
+        perfil: 'candidato',
+        imagem_candidato: '/images/candidato.png',
+        telefone: '(67) 99872-4512',
+        cidade_candidato: 'Nova Andradina',
+        uf_candidato: 'MS'
+      },
+      {
+        nome: 'Ana Carolina',
+        email: 'ana@gmail.com',
+        senha: '123',
+        perfil: 'candidato',
+        imagem_candidato: '/images/candidato.png',
+        telefone: '(67) 99125-7834',
+        cidade_candidato: 'Dourados',
+        uf_candidato: 'MS'
+      },
+      {
+        nome: 'Carlos Henrique',
+        email: 'carlos@gmail.com',
+        senha: '123',
+        perfil: 'candidato',
+        imagem_candidato: '/images/candidato.png',
+        telefone: '(67) 99741-3268',
+        cidade_candidato: 'Campo Grande',
+        uf_candidato: 'MS'
       }
     ])
   },
