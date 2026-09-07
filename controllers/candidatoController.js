d3fc9d0913b3857c162b77937fa268b533ac7b2e
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-09-07 15:11:29 -0400

    Criando a função para o candidato visualizar suas entrevistas. Adicionando função no controler de candidato, chamando a rota em routes, criando e estilizando a view de entrevistas de um candidato

--- controllers/candidatoController.js
+++ controllers/candidatoController.js
@@ -1,4 +1,4 @@
-const { Candidato, Curriculo } = require('../models');
+const { Candidato, Curriculo, Empresa, Candidatura, Vaga, Entrevista } = require('../models');
 
 const getCandidato = async function (req, res, next) {
     const candidato = await Candidato.findByPk(1, { //para buscar somente o usuario 1 enquanto não tem as verificações de login para mostrar somente no perfil o usuario 
@@ -21,5 +21,40 @@ const editar = async function (req, res) {
     res.render('candidatos/editar');
 };
 
+const getEntrevistasCandidato = async function (req, res, next) {
+    try {
+        const entrevistas = await Entrevista.findAll({
+            include: [
+                {
+                    model: Candidatura,
+                    as: 'candidatura',
+                    include: [
+                        {
+                            model: Vaga,
+                            as: 'vaga',
+                            include: [
+                                {
+                                    model: Empresa,
+                                    as: 'empresa'
+                                }
+                            ]
+                        },
+                        {
+                            model: Candidato,
+                            as: 'candidato'
+                        }
+                    ]
+                }
+            ]
+        });
 
-module.exports = { getCandidato, cadastroCandidato, editar };
+        res.render('candidatos/entrevistas', {
+            entrevistas
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { getCandidato, cadastroCandidato, editar, getEntrevistasCandidato };
--- routes/candidatos.js
+++ routes/candidatos.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getCurriculo } = require('../controllers/curriculoController');
-const { getCandidato, cadastroCandidato, editar } = require('../controllers/candidatoController')
-
+const { getCandidato, cadastroCandidato, editar, getEntrevistasCandidato } = require('../controllers/candidatoController')
 
 router.get('/', getCandidato);
 router.get('/cadastro/candidato', cadastroCandidato);
 router.get('/editar', editar);
 router.get('/curriculo', getCurriculo);
+router.get('/entrevistas', getEntrevistasCandidato);
 
 module.exports = router;
